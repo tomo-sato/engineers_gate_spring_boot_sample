3b22c5b9b8d978a13a19d67f145359c29f565306
Tomomichi Sato <tsato@dcworks.jp>
2023-02-15 20:34:22 +0900

    EngineersGate SpringBootサンプル作成。 →改行表示対応。

--- src/main/java/jp/dcworks/engineersgate/egbbs/util/StringUtil.java
+++ src/main/java/jp/dcworks/engineersgate/egbbs/util/StringUtil.java
@@ -2,11 +2,15 @@ package jp.dcworks.engineersgate.egbbs.util;
 
 import java.util.Objects;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.HtmlUtils;
+
 /**
  * 文字列操作に関する機能を提供します。
  *
  * @author tomo-sato
  */
+@Component
 public class StringUtil {
 
 	/** ブランク */
@@ -56,5 +60,32 @@ public class StringUtil {
 	 */
 	public static String toStringOrBlank(final Object obj) {
 		return Objects.toString(obj, BLANK);
+	}
+
+	/**
+	 * 改行コードを<br />タグに変換した情報を返却する。
+	 * @param s 入力文字列
+	 * @return 変換後の文字列を返却します。
+	 */
+	public static String nl2br(String s) {
+		return nl2br(s, true);
+	}
+
+	/**
+	 * 改行コードを<br />、または、<br>タグに変換した情報を返却する。
+	 * @param str 入力文字列
+	 * @param isXhtml XHTML準拠の改行タグの使用する場合はtrueを指定します。
+	 * @return 変換後の文字列を返却します。
+	 */
+	public static String nl2br(String str, boolean isXhtml) {
+		if (str == null || "".equals(str)) {
+			return "";
+		}
+
+		// タグ文字が含まれる場合はエスケープする。
+		String escapeStr = HtmlUtils.htmlEscape(str);
+
+		String tag = isXhtml ? "<br />" : "<br>";
+		return escapeStr.replaceAll("\\r\\n|\\n\\r|\\n|\r", tag);
 	}
 }
